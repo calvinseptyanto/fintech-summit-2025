bed6da8649ec2f33dd3f0ada4a17695db5ec0461
JY <e1237248@u.nus.edu>
2025-01-10 02:25:42 +0800

    add service login, homepage, registration and modify company homepage overview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
+import Registration_service from "./components/Registration_service";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registration" element={<Registration />}/>
+        <Route path="/registration_service" element={<Registration_service />}/>
         <Route path="/homepage" element={<RedirectToHtml page="template_hp.html" />} />
+        <Route path="/homepage_service" element={<RedirectToHtml page="hp_service.html" />} />
       </Routes>
     </Router>
   );
